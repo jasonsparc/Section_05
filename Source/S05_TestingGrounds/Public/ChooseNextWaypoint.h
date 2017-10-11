04612c2e334ed29ba535fec606f95001559cea96
jason <jason.sparc.7@gmail.com>
2017-10-11 20:41:09 +0800

    Test get blackboard data - AI patrolling in C++

--- Source/S05_TestingGrounds/Public/ChooseNextWaypoint.h
+++ Source/S05_TestingGrounds/Public/ChooseNextWaypoint.h
@@ -15,6 +15,9 @@ class S05_TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 	GENERATED_BODY()
 	
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
-	
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector IndexKey;
 	
 };
